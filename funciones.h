3d098e252242785d44c00468648374371e4133d5
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2025-01-10 08:39:47 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,1 +1,32 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
 
+struct Producto {
+    char nombre[50];
+    int cantidad;
+    float precio;
+};
+
+struct Factura {
+    int numeroFactura;
+    char nombre[20];
+    int cedula;
+    int numProductos;
+    float subtotal;
+    float impuestos;
+    float descuentos;
+    float total;
+    struct Producto *productos;
+};
+
+int menu();
+void leerCadena(char *cadena, int num);
+int leerEnteroPositivo(const char *mensaje);
+float leerFloatPositivo(const char *mensaje);
+void calcularTotalFactura(struct Factura *factura, float impuesto, float descuento);
+void imprimirFactura(struct Factura *factura);
+void saveFactura(struct Factura *factura);
+void createFactura(int *contadorFactura);
+void readFactura();
+
+#endif // FUNCIONES_H
