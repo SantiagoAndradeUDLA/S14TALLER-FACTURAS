27b5e18e4b911e33240b2a94814be9fd3a8b7794
SantiagoAndradeUDLA <santiago.andrade.bucheli@udla.edu.ec>
2025-01-10 08:39:12 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include "funciones.h"
 
+int menu() {
+    int opcion;
+    printf("\n--- MENU PRINCIPAL ---\n");
+    printf("1. Crear factura\n");
+    printf("2. Leer última factura\n");
+    printf("3. Salir\n");
+    printf("Opcion: ");
+    scanf("%d", &opcion);
+    return opcion;
+}
+
+int main() {
+    int opcion;
+    int contadorFactura = 0;
+
+    do {
+        opcion = menu();
+        switch (opcion) {
+        case 1:
+            createFactura(&contadorFactura);
+            break;
+        case 2:
+            readFactura();
+            break;
+        case 3:
+            printf("Saliendo del programa...\n");
+            break;
+        default:
+            printf("Opción inválida.\n");
+        }
+    } while (opcion != 3);
+
+    return 0;
+}
